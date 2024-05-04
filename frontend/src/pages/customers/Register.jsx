f9cb9e191ffc1b48e726545450945aeab1105d83
IT21255588 <it21255588@my.sliit.lk>
2024-05-04 20:23:08 +0530

    change

--- frontend/src/pages/customers/Register.jsx
+++ frontend/src/pages/customers/Register.jsx
@@ -241,7 +241,7 @@ export default function SignUp() {
             <Grid container justifyContent="flex-end">
               <Grid item>
                 <Link href="/customers/login" variant="body2">
-                  Already have an account? Sign in
+                  Already have an account? Sign in..
                 </Link>
               </Grid>
             </Grid>
