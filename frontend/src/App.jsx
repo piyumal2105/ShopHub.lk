4db6e5ac707eef76e92ae1a15957ddafd76491b0
IT21077524 <99633234+IT21077524@users.noreply.github.com>
2024-05-05 02:15:42 +0530

    events calender and promotions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,27 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import Customers from "./components/Customers/Customers";
 import AdminLogin from "./components/AdminLogin/AdminLogin";
-
+import Promotions from "./components/Promotions/Promotions";
+import AdminPromotions from "./components/Promotions/AdminPromotions";
+import { QueryClientProvider,QueryClient } from "react-query";
+import AdminEvent from "./components/Events/AdminEvents";
+import MyCalendar from "./components/Events/events";
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/customers" element={<Customers />} />
         <Route path="/admin/login" element={<AdminLogin />} />
+        <Route path="/promotions" element={<Promotions />} />
+        <Route path="/adminpromotions" element={<AdminPromotions />} />
+        <Route path="/adminevents" element={<AdminEvent />} />
+        <Route path="/events" element={<MyCalendar />} />
       </Routes>
     </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
