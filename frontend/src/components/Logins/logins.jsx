785be7624b686c18b6a5a2f3a60b5607417cac93
IT21057892 <99260819+IT21057892@users.noreply.github.com>
2024-06-15 00:02:18 +0530

    Add home page button to login page

--- frontend/src/components/Logins/logins.jsx
+++ frontend/src/components/Logins/logins.jsx
@@ -3,22 +3,37 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import AdminLogIn from "../AdminLogin/AdminLogin.jsx";
 import ShopLogIn from "../ShopMemberLogin/ShopMemberLogin.jsx";
+import Button from "react-bootstrap/Button";
 
 function Logins() {
   return (
-    <div
-      className="login-wrapper"
-      style={{ padding: "20px", marginTop: "100px", marginLeft: "100px" }}
-    >
-      <Row className="justify-content-md-center g-4">
-        <Col md={6}>
-          <AdminLogIn />
-        </Col>
-        <Col md={6}>
-          <ShopLogIn />
-        </Col>
-      </Row>
-    </div>
+    <>
+      <div
+        className="login-wrapper"
+        style={{
+          padding: "20px",
+          marginTop: "100px",
+          marginLeft: "100px",
+        }}
+      >
+        <Row className="justify-content-md-center g-4">
+          <Col md={6}>
+            <AdminLogIn />
+          </Col>
+          <Col md={6}>
+            <ShopLogIn />
+          </Col>
+        </Row>
+        <br />
+        <br />
+        <Button
+          style={{ backgroundColor: "black", alignItems: "self-end" }}
+          href="/"
+        >
+          Home Page
+        </Button>
+      </div>
+    </>
   );
 }
 
